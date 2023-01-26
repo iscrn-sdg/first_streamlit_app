eb60c8cd3a66d5141dda4458ac9429c5f0e631f8
iscrn-sdg <112474637+iscrn-sdg@users.noreply.github.com>
2023-01-26 16:18:15 +0100

    aggiunta menu colazione

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
